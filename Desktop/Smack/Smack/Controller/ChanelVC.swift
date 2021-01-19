d30f4bf70baa3e6ff7c5a1245c84a7dff5cc6938
Amritpal Singh <ammu.uppal@hotmail.com>
2021-01-19 00:57:19 -0800

    fixed errors

---
+++ Desktop/Smack/Smack/Controller/ChanelVC.swift
@@ -0,0 +1,31 @@
+//
+//  ChanelVC.swift
+//  Smack
+//
+//  Created by Singh Amritpal on 2021-01-18.
+//
+
+import UIKit
+
+class ChanelVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Desktop/Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Singh Amritpal on 2021-01-18.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController, action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+    
+
+
+}
